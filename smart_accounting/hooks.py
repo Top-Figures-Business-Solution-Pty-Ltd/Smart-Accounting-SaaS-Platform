a2a73e16420b6130b62956156e2761e75627a921
J3ffr33y <zigengwang464@gmail.com>
2025-10-21 13:48:32 +1100

    Added some doctypes that needed for client-centric & contact-centric tasks

--- smart_accounting/hooks.py
+++ smart_accounting/hooks.py
@@ -270,7 +270,7 @@ fixtures = [
 	},
 	{
 		"doctype": "DocType",
-		"filters": [["name", "in", ["User Preferences", "Partition", "Task Role Assignment", "Task Software", "Task Company Tag", "Board Column", "Board Cell", "Service Line", "Review Note", "Engagement", "Client Group", "Combination View", "Combination View Board", "Task Communication Method", "Contact Social"]]]
+		"filters": [["name", "in", ["User Preferences", "Partition", "Task Role Assignment", "Task Software", "Task Company Tag", "Board Column", "Board Cell", "Service Line", "Review Note", "Engagement", "Client Group", "Combination View", "Combination View Board", "Task Communication Method", "Contact Social", "Client Referral", "Client Project Info"]]]
 	}
 	# 不导出 Dashboard Chart, Number Card 等可能包含标准内容的 DocType
 ]
